b005a55658a07dc2f6e08afee2c4d3238e2b7be7
Kristofer Rye <kristofer.rye@gmail.com>
2018-10-16 07:23:27 -0500

    ping: Write first tests

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,20 @@ fn ping() -> &'static str {
 	"pong"
 }
 
+mod test {
+	use super::rocket;
+	use rocket::local::Client;
+	use rocket::http::Status;
+
+	#[test]
+	fn ping_responds_with_pong() {
+		let client = Client::new(rocket()).expect("valid rocket instance");
+		let mut response = client.get("/ping").dispatch();
+
+		assert_eq!(response.status(), Status::Ok);
+		assert_eq!(response.body_string(), Some("pong".into()));
+	}
+}
 
 fn rocket() -> rocket::Rocket {
 	rocket::ignite()
