209ce72b8794fa81f6b0761ba490a9344dd15ba0
Kristofer Rye <kristofer.rye@gmail.com>
2018-10-16 07:22:01 -0500

    main: Add a /ping route
    This is the first _proper_ route for the API server, and simply
    returns with the string "pong" in cleartext.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+#[get("/ping")]
+fn ping() -> &'static str {
+	"pong"
+}
+
 fn main() {
-	println!("Hello, world!")
+	rocket::ignite()
+		.mount("/", routes![ping])
+		.launch();
 }
