a5c018eaf1ba102b8628ec379c798972d2a58ddb
Kristofer Rye <kristofer.rye@gmail.com>
2018-12-23 21:35:39 -0600

    t/ping_pong: Nest namespaces
    I can't find a way to do this with ::, but I'll dig a bit deeper just
    to see.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- tests/ping_pong.rs
+++ tests/ping_pong.rs
@@ -1,15 +1,17 @@
 extern crate rocket;
 
-mod ping_pong {
-	use rocket::http::Status;
-	use rocket::local::Client;
+mod routes {
+	mod ping {
+		use rocket::http::Status;
+		use rocket::local::Client;
 
-	#[test]
-	fn ping_responds_with_pong() {
-		let client = Client::new(data_server::server()).expect("valid rocket instance");
-		let mut response = client.get("/ping").dispatch();
+		#[test]
+		fn responds_with_pong() {
+			let client = Client::new(data_server::server()).expect("valid rocket instance");
+			let mut response = client.get("/ping").dispatch();
 
-		assert_eq!(response.status(), Status::Ok);
-		assert_eq!(response.body_string(), Some("pong".into()));
+			assert_eq!(response.status(), Status::Ok);
+			assert_eq!(response.body_string(), Some("pong".into()));
+		}
 	}
 }
