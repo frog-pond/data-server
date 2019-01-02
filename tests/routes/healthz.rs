eb9fa55ba9ad24046a817347ca988dd83174b357
Kristofer Rye <kristofer.rye@gmail.com>
2019-01-02 08:58:48 -0600

    t/healthz: Add when_healthy_responds_healthily test
    A great example of mocking.  We just need to ensure that we use
    mock_safe properly.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- tests/routes/healthz.rs
+++ tests/routes/healthz.rs
@@ -1,6 +1,24 @@
 use super::*;
+use mocktopus::mocking::*;
 
 use data_server::routes::healthz::*;
+
+#[test]
+fn when_healthy_responds_healthily() {
+	use data_server::routes::healthz::Health;
+	data_server::routes::healthz::get_health.mock_safe(|| MockResult::Return(Health { code: true }));
+
+	let rocket = rocket::ignite().mount("/", routes![healthz]);
+	let client = Client::new(rocket).expect("valid rocket instance");
+	let mut response = client.get("/healthz").dispatch();
+
+	assert_eq!(response.status(), Status::Ok);
+
+	let response: String = response.body_string().unwrap();
+	let response: Health = serde_json::from_str(&response).unwrap();
+
+	assert_eq!(response.code, true);
+}
 
 #[test]
 fn responds_with_correct_headers() {
