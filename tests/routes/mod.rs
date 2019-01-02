32cd4613f367aadd69e18ec6855c95253d8fbe4a
Kristofer Rye <kristofer.rye@gmail.com>
2019-01-02 09:13:45 -0600

    t/routes: Add a comment explaining why we have mods in t/r/mod.rs
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- tests/routes/mod.rs
+++ tests/routes/mod.rs
@@ -3,5 +3,11 @@
 use rocket::http::Status;
 use rocket::local::Client;
 
+// In order to get your test module to run, you need to add a mod <file.rs>.
+// Note that .rs files in this directory are each given their own "module" that
+// is named appropriately, so by adding a .rs file you add a module.  You
+// should really like that.
+//
+// Adding `mod` <filename sans .rs> will do.
 mod healthz;
 mod ping;
